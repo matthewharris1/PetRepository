7a3bc5e70a3b1d9246a792b7ceaf22062eecd41d
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2018-02-07 11:46:13 +0000

    Added cat class, turned dog/cat into case class

--- src/main/scala/petRepo/PetRepo.scala
+++ src/main/scala/petRepo/PetRepo.scala
@@ -1,15 +1,15 @@
 package petRepo
 
-import petRepo.repositories.{Dog, PetRepository}
+import petRepo.repositories.{Cat, Dog, PetRepository}
 
 
 object PetRepo extends App {
 
-  val pets = PetRepository.all()
-  println(pets)
-
-  val dogs = for(i <- 1 to 100) yield new Dog(s"Dog $i")
+  val dogs = for(i <- 1 to 100) yield Dog(s"Dog $i")
   PetRepository.add(dogs: _*)
+
+  val cats = for(i <- 1 to 100) yield Cat(s"Cat $i")
+  PetRepository.add(cats: _*)
 
   val p2 = PetRepository.all()
   println(p2)
--- src/main/scala/petRepo/repositories/Cat.scala
+++ src/main/scala/petRepo/repositories/Cat.scala
@@ -5,6 +5,6 @@ import petRepo.Pet
 /**
   * Created by digital032676 on 07/02/18.
   */
-case class Dog (name: String) extends Pet{
+case class Cat (name: String) extends Pet{
 
 }
